ee6302d66ab4603a3fe53f6ece796f1c63b46a8e
akennedy94 <71675206+akennedy94@users.noreply.github.com>
2021-03-18 11:17:41 -0500

    Loading Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+import "./components/componentCSS/App.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import PodcastForm from "./components/PodcastForm";
